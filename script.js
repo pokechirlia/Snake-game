16f607d844121c3c60f74b783a6ca1d753a6b3b3
pokechirlia <chi.k.tran2000@gmail.com>
2022-11-27 16:16:41 +1100

    moving snake now

--- script.js
+++ script.js
@@ -3,19 +3,21 @@ const gameBox = document.getElementById("game-box");
 const ctx = gameBox.getContext("2d");
 
 //attributes from the game
-let speed = 8;
+let speed = 10;
 
 let tileCount = 20; //canvas 20x20
 let tileSize = (gameBox.width / tileCount) - 2; //size slightly smaller
 let headX = 10;
 let headY = 10;
+let xVelocity = 0, yVelocity = 0;
 
 //game loop update every interval
 function drawGame(){
     console.log("again?");
     clearScreen();
+    updateSnakePosition();
     drawSnake();
-    //setTimeout(drawGame, 1000/speed);
+    setTimeout(drawGame, 2000/speed);
 }
 
 function clearScreen(){
@@ -27,5 +29,46 @@ function drawSnake(){
     ctx.fillStyle = "red";
     ctx.fillRect(headX * tileCount, headY*tileCount, tileSize, tileSize);
 }
+
+function updateSnakePosition(){
+    headX += xVelocity;
+    headY += yVelocity;
+    //console.log("ayo");
+}
+
+function newGame(){
+}
+
+//check key input
+document.addEventListener("keydown", keyDown);
+
+function keyDown(event){
+    //up key
+    if(event.keyCode == 38){
+        yVelocity = -1;
+        xVelocity = 0;
+    }
+
+    //down key
+    if(event.keyCode == 40){
+        yVelocity = 1;
+        xVelocity = 0;
+    }
+
+    //left key
+    if(event.keyCode == 37){
+        yVelocity = 0;
+        xVelocity = -1;
+    }
+
+    //right key
+    if(event.keyCode == 39){
+        yVelocity = 0;
+        xVelocity = 1;
+    }
+
+    
+}
+
 
 drawGame();
