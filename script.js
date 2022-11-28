b2674528036a28184af4c276d086dd7839fab3c1
pokechirlia <chi.k.tran2000@gmail.com>
2022-11-28 14:51:49 +1100

    game is completed
    there may be bug need to be fixed though

--- script.js
+++ script.js
@@ -2,6 +2,9 @@
 const gameBox = document.getElementById("game-box");
 const ctx = gameBox.getContext("2d");
 const gameScoreBox = document.getElementById("score");
+const messageBox = document.getElementById("message-box");
+const speedBox = document.getElementById("speed");
+
 
 //snake class
 class SnakePart{
@@ -12,7 +15,8 @@ class SnakePart{
 }
 
 //attributes from the game
-let speed = 10;
+let speed = 5;
+speedBox.defaultValue = speed;
 let tileCount = 20; //canvas 20x20
 let tileSize = (gameBox.width / tileCount) - 2; //size slightly smaller
 let headX = 10;
@@ -21,25 +25,38 @@ let foodX = 5;
 let foodY = 5;
 let xVelocity = 0, yVelocity = 0;
 
-const snakeParts = [];
+let snakeParts = [];
 let score = 0;
 let initialLength = 2;
 let highestScore = 0;
+let edge = 3;
+let message;
 
 //game loop update every interval
 function drawGame(){
     
+    speed = speedBox.value; 
+
     clearScreen();
     updateSnakePosition();
     checkFoodEaten();
     drawFood();
     drawSnake();
-    setTimeout(drawGame, 2000/speed);
+
+    let gameOver = checkGameOver();
+    updateMessage(gameOver);
+    if(gameOver)
+        return;
+
+    setTimeout(drawGame, 1000/speed);
 }
 
 function clearScreen(){
-    ctx.fillStyle = "rgb(135, 110, 64)";
+    ctx.fillStyle = "black";
     ctx.fillRect(0,0, gameBox.width, gameBox.height);
+
+    ctx.fillStyle = "rgb(135, 110, 64)";
+    ctx.fillRect(edge,edge, gameBox.width-edge*2, gameBox.height-edge*2);
 }
 
 function drawSnake(){
@@ -83,17 +100,92 @@ function checkFoodEaten(){
 }
 
 function randomFoodPosition(){
-    foodX = Math.floor(Math.random() * tileCount);
-    foodY = Math.floor(Math.random() * tileCount);
+
+    //random so that the food is not too close to the edge
+    foodX = Math.floor(Math.random() * (tileCount-2) + 1);
+    foodY = Math.floor(Math.random() * (tileCount-2) + 1);
+}
+
+function updateMessage(gameOver){
+    
+    if(gameOver){
+        message = "Did I just hit something? GAME OVER :("
+    }
+    else{
+
+        if(score > highestScore)
+            message = "I just pass my highest score! Keep going!";
+
+        if(score == 1)
+            message = "Yay! My first score!";
+
+        if(score == 5)
+            message = "Yummm so gooooood!!!";
+
+        if(score == 10)
+            message = "We are having a party today!!";
+
+        if(score == 20)
+            message = "I'm starting to get full now...";
+
+        if(score == 40)
+            message = "We actually reach this far?";
+
+    }
+
+
+    messageBox.innerHTML = message;
+}
+
+function checkGameOver(){
+    let gameOver = false;
+
+    //game cannot be over if it is not started yet
+    if(yVelocity == 0 && xVelocity == 0)
+        return gameOver;
+
+    if(headX <= 0 || headX >= tileCount-1 || headY <= 0 || headY >= tileCount-1)
+        gameOver = true;
+
+    for(let i = 0; i < snakeParts.length-1; i++){
+        let part = snakeParts[i];
+        if(headX == part.x && headY == part.y)
+        {
+            gameOver = true;
+            break;
+        }
+    }
+
+    if(gameOver)
+    {
+        if(score > highestScore)
+        {
+            highestScore = score;
+            
+        }
+        gameScoreBox.innerHTML = "Highest score: " + highestScore;
+    }
+
+    return gameOver;
 }
 
 function newGame(){
+
+    message = "Hello, welcome to my snake game!";
+
+    //reset all attributes
+    headX = 10;
+    headY = 10;
+    xVelocity = 0;
+    yVelocity = 0;
+
+    snakeParts = [];
+    score = 0;
+
     randomFoodPosition();
     drawGame();
-    if(score > highestScore)
-        highestScore = score;
 
-    gameScoreBox.innerHTML = "Highest score: " + highestScore;
+    
 }
 
 //check key input
